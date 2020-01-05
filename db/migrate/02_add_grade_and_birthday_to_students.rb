deebc6da44bdf9b37424c8b5869cf67654e0ed26
Pancho Labrador <plabradorjr@gmail.com>
2020-01-05 05:44:22 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthday_to_students.rb
+++ db/migrate/02_add_grade_and_birthday_to_students.rb
@@ -1,8 +1,8 @@
-class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
 
   def change
     add_column :students, :grade, :integer
-    add_column :students, :birthdate, :string 
+    add_column :students, :birthdate, :string
   end
 
 end
